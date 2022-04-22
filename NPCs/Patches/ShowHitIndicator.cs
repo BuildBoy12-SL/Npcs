791511f7cb28f2e9dc502929c42448aa9c4e7010
Build <jwbradley2003@gmail.com>
2022-04-22 01:34:29 -0400

    Patch StandardHitregBase::ShowHitIndicator

---
+++ NPCs/Patches/ShowHitIndicator.cs
@@ -0,0 +1,47 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ShowHitIndicator.cs" company="Build">
+// Copyright (c) Build. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace NPCs.Patches
+{
+#pragma warning disable SA1118
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+    using HarmonyLib;
+    using InventorySystem.Items.Firearms.Modules;
+    using NorthwoodLib.Pools;
+    using NPCs.API;
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patches <see cref="StandardHitregBase.ShowHitIndicator"/> to prevent sending hit indicators to null connections.
+    /// </summary>
+    [HarmonyPatch(typeof(StandardHitregBase), nameof(StandardHitregBase.ShowHitIndicator))]
+    internal static class ShowHitIndicator
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            Label returnLabel = generator.DefineLabel();
+
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ldloc_0);
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloc_0).MoveLabelsFrom(newInstructions[index]),
+                new CodeInstruction(OpCodes.Call, Method(typeof(Extensions), nameof(Extensions.IsNpc), new[] { typeof(ReferenceHub) })),
+                new CodeInstruction(OpCodes.Brtrue_S, returnLabel),
+            });
+
+            newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+}
