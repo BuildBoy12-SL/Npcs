4e9fddf126e212317bd6bdee4f0876075e51e942
Build <jwbradley2003@gmail.com>
2022-09-12 18:31:53 -0400

    No longer force lite on class change

--- NPCs/Patches/SetClass.cs
+++ NPCs/Patches/SetClass.cs
@@ -8,20 +8,45 @@
 namespace NPCs.Patches
 {
 #pragma warning disable SA1118
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
     using HarmonyLib;
+    using NorthwoodLib.Pools;
     using NPCs.API;
     using UnityEngine;
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
-    /// Patches <see cref="CharacterClassManager.SetPlayersClass"/> to force the lite argument to be true if the target is an npc.
+    /// Patches <see cref="PlayerMovementSync.OnPlayerClassChange"/> to only set the position of npcs.
     /// </summary>
-    [HarmonyPatch(typeof(CharacterClassManager), nameof(CharacterClassManager.SetPlayersClass))]
+    [HarmonyPatch(typeof(PlayerMovementSync), nameof(PlayerMovementSync.OnPlayerClassChange))]
     internal static class SetClass
     {
-        private static void Prefix(GameObject ply, ref bool lite)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            if (ply.IsNpc())
-                lite = true;
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            Label continueLabel = generator.DefineLabel();
+            newInstructions[0].labels.Add(continueLabel);
+
+            newInstructions.InsertRange(0, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(PlayerMovementSync), nameof(PlayerMovementSync.gameObject))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(Extensions), nameof(Extensions.IsNpc), new[] { typeof(GameObject) })),
+                new CodeInstruction(OpCodes.Brfalse_S, continueLabel),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldarg_1),
+                new CodeInstruction(OpCodes.Ldarg_2),
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Callvirt, Method(typeof(PlayerMovementSync), nameof(PlayerMovementSync.OverridePosition), new[] { typeof(Vector3), typeof(PlayerMovementSync.PlayerRotation?), typeof(bool) })),
+                new CodeInstruction(OpCodes.Ret),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
     }
 }
