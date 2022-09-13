a11b3f579c082709a52eadc322bf686c5742f4aa
Build <jwbradley2003@gmail.com>
2022-09-13 01:00:43 -0400

    radio nullref fix

---
+++ NPCs/Patches/SendRadioStatus.cs
@@ -0,0 +1,49 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SendRadioStatus.cs" company="Build">
+// Copyright (c) Build. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace NPCs.Patches
+{
+#pragma warning disable SA1118
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+    using HarmonyLib;
+    using InventorySystem.Items.Radio;
+    using Mirror;
+    using NorthwoodLib.Pools;
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Prevents sending the status of radios to a null connection.
+    /// </summary>
+    [HarmonyPatch(typeof(RadioItem), nameof(RadioItem.SendStatusMessage))]
+    internal static class SendRadioStatus
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            Label continueLabel = generator.DefineLabel();
+
+            const int offset = 1;
+            int index = newInstructions.FindIndex(instruction => instruction.OperandIs(PropertyGetter(typeof(NetworkBehaviour), nameof(NetworkBehaviour.connectionToClient)))) + offset;
+            newInstructions[index].labels.Add(continueLabel);
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Brtrue_S, continueLabel),
+                new CodeInstruction(OpCodes.Pop),
+                new CodeInstruction(OpCodes.Ret),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+}
